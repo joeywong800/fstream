a6ebacd27c08821af1eb9663d549c7f3d6755959
altran1800 <altran1800@gmail.com>
2025-08-14 20:53:09 -0500

    sync and changes

--- public/config.js
+++ public/config.js
@@ -1,10 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://1baebdd1.mootmoot.pages.dev",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MjA5NzNjNGE2NjI4OTYzZDhmYzFjY2U3OWM3MzUwMiIsIm5iZiI6MTc1NTIxODYxOC40OTIsInN1YiI6IjY4OWU4MmJhMmRjZGJmMWYyNDZkODJiYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sz3TCDE8-ru9Ozq9exBpUtOeK1_pO9l_cR_tVlGz0y8",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
